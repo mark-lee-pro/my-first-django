10fb67bfe637179eeb3d97309c8aceac942bdf5c
mark-lee-pro <smlee7576@hotmail.com>
2020-10-13 10:53:56 +0900

    add env/

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\markl\documents\visual studio 2013\Projects\DjangoWebProject2\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
